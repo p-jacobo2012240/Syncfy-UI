b056ecbae5aa0b9544b762e534efb03f958f169c
Pablo Jacobo <pablojacobo1998@gmail.com>
2018-08-06 12:17:12 -0600

    README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 # API 
 
-API REST Desplegada en Heroku, construida como Back-End para solucionar un conjunto de requerimientos, las técnologias utilizadas fueron seleccionadas por su alta escalabilidad y nivel de Performance.
+API REST. Contruida con Node.Js, Express.Js Como Tecnologias de Back-End. 
 
 [![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy?template=https://github.com/p-jacobo2012240/API)
--- config/config.js
+++ config/config.js
@@ -8,9 +8,8 @@ module.exports.CLIENT_ID = '281371971439-i03uo7js2cekjs9a21kloh7luj3sh70h.apps.g
 // ============================
 process.env.PORT = process.env.PORT || 3000;
 
-
 // ============================
-//  Entorno
+//  ENVIROMENT
 // ============================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
@@ -20,7 +19,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/steam-v1';
+    urlDB = 'mongodb://localhost:27017/steam';
 } else {
     urlDB = 'mongodb://pablo:jswuupa98@ds141661.mlab.com:41661/steam';
 }
