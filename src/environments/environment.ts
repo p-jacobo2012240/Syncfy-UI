5a58eca9f18125a1e482c84a69dbcce6cf2f4920
p-jacobo2012240 <pablojacobo1998@gmail.com>
2022-10-11 00:31:40 -0600

    changed navbar for improvements into sidenav and better nav actions

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,6 +11,7 @@ export const environment = {
 };
 
 
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
