813a2a64c47e783a11aed5a2e31fe95d79db6fd0
p-jacobo2012240 <pablojacobo1998@gmail.com>
2022-10-10 22:48:24 -0600

    all charts was added for notifications and alerts

--- src/app/dashboard/dashboard.routing.ts
+++ src/app/dashboard/dashboard.routing.ts
@@ -6,12 +6,17 @@ import { SettingOfMetricsComponent } from './setting-of-metrics/setting-of-metri
 
 // Import the authentication guard
 import { AuthGuard } from '@auth0/auth0-angular';
+import { MetricsComponent } from './metrics/metrics.component';
 
 const dashboardRoutes: Routes = [
     { 
         path: 'dashboard', 
         component: DashboardComponent,
         children: [
+            {
+                path: '',
+                component: MetricsComponent
+            },
             { 
                 path: 'metrics-settings', 
                 component: SettingOfMetricsComponent  
