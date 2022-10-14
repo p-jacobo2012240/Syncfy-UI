41d0cd20b64efc173e86d21e91eec283f58aae2a
p-jacobo2012240 <pablojacobo1998@gmail.com>
2022-10-13 22:29:18 -0600

    was fixed url ocnfig

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  auth0: {
+    domain: 'quinx2.auth0.com',
+    clientId: 'V5Wrt8I5aUkWvDWm4kIeOPW508mH4bNX'
+  },
+  syncfyManagement: {
+    apiURL: 'http://localhost',
+    port: '8081'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   auth0: {
     domain: 'quinx2.auth0.com',
     clientId: 'V5Wrt8I5aUkWvDWm4kIeOPW508mH4bNX'
