a30a39272bf7f56b7a7dfd3eb7de23ecf4919e51
p-jacobo2012240 <pablojacobo1998@gmail.com>
2022-10-13 22:10:49 -0600

    fixed enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   auth0: {
-    domain: 'quinx2.auth0.com',
-    clientId: 'V5Wrt8I5aUkWvDWm4kIeOPW508mH4bNX'
+    domain: 'XXX-XXX-XXX',
+    clientId: 'XXX-XXX-XXX'
   },
   syncfyManagement: {
     apiURL: 'http://localhost',
